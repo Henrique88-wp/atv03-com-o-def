525be228e04149541b6ef849f9fcfd1f79f6d2fc
Henrique88-wp <phdsn411@gmail.com>
2024-10-16 14:54:09 -0300

    atv03 com def

---
+++ atv02comdef.py
@@ -0,0 +1,16 @@
+# Crie duas funções:
+# - calcular_area_base, que recebe o raio de um círculo e retorna sua área.
+# - calcular_volume_cilindro, que utiliza a função calcular_area_base para calcular o volume de um cilindro dado o raio e a altura.import math
+import math
+def calcular_area_base(raio):
+    return math.pi * raio ** 2
+
+def calcular_volume_cilindro(raio, altura):
+    area_base = calcular_area_base(raio)
+    return area_base * altura
+
+
+raio = float(input("coloque o raio:"))
+altura = float(input("coloque a altura:"))
+volume = calcular_volume_cilindro(raio, altura)
+print(f"O volume do cilindro é: {volume:.2f}")
